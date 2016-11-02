d154635adff532a35237d907d48e750b73289793
Gabriel Salas <ggsalas@gmail.com>
2016-11-02 14:46:14 -0300

    finish class 8

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,8 @@ class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Catch 
+        <h1>
+          Catch 
           <span className="ofThe">
             <span className="of">of</span>
             <span className="the">the</span>
