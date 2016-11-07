e4d40bb5c64446d0e71c5c82f82df780cb75f1c8
Gabriel Salas <ggsalas@gmail.com>
2016-11-07 16:59:20 -0300

    finish class 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,16 +2,35 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault()
+
+    console.log('Funciona')
+
+    // Grab the text from the box
+    const storeId = this.storeInput.value
+    console.log(storeId)
+
+    // Going from "/" to "/store/storeID"
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)} >
         {/* This is a Comment */}
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+// Create "router" context to StorePicker component
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker 
