7edc91c21c2fa8ef075388a3ad852ab2e5b7c7ac
Gabriel Salas <ggsalas@gmail.com>
2016-11-12 03:33:39 -0300

    finisf class 13: States in React

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,31 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+
+    // llamar al método addFish
+    this.addFish = this.addFish.bind(this)
+  }
+
+  addFish(fish) {
+    // Pasar el estado de fishes a una variable "fishes"
+    const fishes = {...this.state.fishes}
+
+    // agregar un nuevo pececillo
+    const timestamp = Date.now()
+    fishes[`fish-${timestamp}`] = fish
+
+    // avisar a react que actualice el estado de fishes
+    this.setState({fishes: fishes})
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +36,7 @@ class App extends React.Component {
           <Header tagline="esta es la tagline" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
