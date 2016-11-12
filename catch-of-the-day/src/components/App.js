edda330b2270dd135a3b514ee117e5da31977ba0
Gabriel Salas <ggsalas@gmail.com>
2016-11-12 04:08:10 -0300

    finish class 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   constructor() {
@@ -15,6 +16,8 @@ class App extends React.Component {
 
     // llamar al método addFish
     this.addFish = this.addFish.bind(this)
+    // .. y al método loadSamples
+    this.loadSamples = this.loadSamples.bind(this)
   }
 
   addFish(fish) {
@@ -29,6 +32,12 @@ class App extends React.Component {
     this.setState({fishes: fishes})
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -36,7 +45,7 @@ class App extends React.Component {
           <Header tagline="esta es la tagline" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
