3f3f6b6546abc41a52df7941e174f3956500cb5c
Gabriel Salas <ggsalas@gmail.com>
2016-12-12 20:33:46 -0300

    image display styles

--- finalProjectReact/src/components/SiteFile.js
+++ finalProjectReact/src/components/SiteFile.js
@@ -5,7 +5,7 @@ class SiteFile extends React.Component {
     const imageExt = ['png', 'jpg', 'jpeg', 'tiff', 'gif', 'exif', 'svg', 'bmp', 'webp']
     const fileExt = this.props.fileName.split('.').slice(-1)[0].toLowerCase()
     const iframe = <iframe className="siteFile-iframe" src={`https://docs.google.com/viewer?url=${this.props.fileURL}&embedded=true`}></iframe>
-    const image = <img src={this.props.fileURL} alt={this.props.fileName}/>
+    const image = <img className="siteFile-image" src={this.props.fileURL} alt={this.props.fileName}/>
 
     return (
       <div className="siteFile">
@@ -13,7 +13,9 @@ class SiteFile extends React.Component {
           <a href={this.props.fileURL} className="button button-download">Descargar</a>
           <button type="button" onClick={this.props.fileClose} className="button button-close"></button>
         </div>
-        {imageExt.indexOf(fileExt) == -1 ? iframe : image}
+        <div className="siteFile-preview">
+          {imageExt.indexOf(fileExt) == -1 ? iframe : image}
+        </div>
       </div>
     )
   }
