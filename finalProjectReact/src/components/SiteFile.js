e7bac3ac76dbf492d533cda38d49b7afdac0c996
Gabriel Salas <ggsalas@gmail.com>
2016-12-12 21:05:10 -0300

    video preview added

--- finalProjectReact/src/components/SiteFile.js
+++ finalProjectReact/src/components/SiteFile.js
@@ -3,10 +3,13 @@ import React from 'react'
 class SiteFile extends React.Component {
   render() {
     const imageExt = ['png', 'jpg', 'jpeg', 'tiff', 'gif', 'exif', 'svg', 'bmp', 'webp']
+    const videoExt = ['mp4', 'webm', 'ogv', '3gp', 'ogg', 'mp3', 'wave', 'wav', 'aac'] //mpeg not supported?
     const fileExt = this.props.fileName.split('.').slice(-1)[0].toLowerCase()
+
     const iframe = <iframe className="siteFile-iframe" src={`https://docs.google.com/viewer?url=${this.props.fileURL}&embedded=true`}></iframe>
     const image = <img className="siteFile-image" src={this.props.fileURL} alt={this.props.fileName}/>
-
+    const video = <video className="siteFile-video" src={this.props.fileURL} controls autoPlay>Tu navegador no implementa el elemento <code>video</code>. </video>
+    
     return (
       <div className="siteFile">
         <div className="siteFile-menu">
@@ -14,7 +17,7 @@ class SiteFile extends React.Component {
           <button type="button" onClick={this.props.fileClose} className="button button-close"></button>
         </div>
         <div className="siteFile-preview">
-          {imageExt.indexOf(fileExt) == -1 ? iframe : image}
+          {imageExt.indexOf(fileExt) >= 0 ? image : videoExt.indexOf(fileExt) >= 0 ? video : iframe}
         </div>
       </div>
     )
