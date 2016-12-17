98ec6df66f2da6fdee0119e85a4ea29667877c38
Gabriel Salas <ggsalas@gmail.com>
2016-12-17 13:45:53 -0300

    css transitions with React-CSSTransitionGroup

--- finalProjectReact/src/components/SiteFile.js
+++ finalProjectReact/src/components/SiteFile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 class SiteFile extends React.Component {
   constructor(props, context) {
@@ -30,25 +31,32 @@ class SiteFile extends React.Component {
 
     const imageExt = ['png', 'jpg', 'jpeg', 'tiff', 'gif', 'exif', 'svg', 'bmp', 'webp']
     const videoExt = ['mp4', 'webm', 'ogv', '3gp', 'ogg', 'mp3', 'wave', 'wav', 'aac'] //mpeg not supported?
+    // TODO file extensions to display iframe, else {display message and download button}
     const fileExt = this.state.fileSelected.name.split('.').slice(-1)[0].toLowerCase()
 
-    const iframe = this.state.fileUrl ? <iframe className="siteFile-iframe" src={`https://docs.google.com/viewer?url=${this.state.fileUrl}&embedded=true`}></iframe> : ''
-    const image = <img className="siteFile-image" src={this.state.fileUrl} alt={this.props.fileName}/>
-    const video = <video className="siteFile-video" src={this.state.fileUrl} controls autoPlay>Tu navegador no implementa el elemento <code>video</code>. </video>
+    const iframe = this.state.fileUrl ? <iframe key={this.state.fileSelected.id} className="siteFile-iframe" src={`https://docs.google.com/viewer?url=${this.state.fileUrl}&embedded=true`}></iframe> : ''
+    const image = <img key={this.state.fileSelected.id} className="siteFile-image" src={this.state.fileUrl} alt={this.props.fileName}/>
+    const video = <video key={this.state.fileSelected.id} className="siteFile-video" src={this.state.fileUrl} controls autoPlay>Tu navegador no implementa el elemento <code>video</code>. </video>
     
     return (
       <div className="siteFile">
         <div className="siteFile-menu">
-          <a href={this.props.fileUrl} className="button button-download">Descargar</a>
+          <a href={this.state.fileUrl} className="button button-download">Descargar</a>
           <div>
           {filePrevious !== '' ? <button className="button" onClick={() => this._fileGet({path: filePrevious.id, fileSelected: filePrevious})}>&#8592; Anterior</button> : ''}
           {fileNext !== '' ? <button className="button" onClick={() => this._fileGet({path: fileNext.id, fileSelected: fileNext})}>Siguiente &#8594;</button> : ''}
           </div>
           <button type="button" onClick={this.props.fileClose} className="button button-close"></button>
         </div>
-        <div className="siteFile-preview">
+        <CSSTransitionGroup
+          component="div"
+          className="siteFile-preview"
+          transitionName="siteFile-preview"
+          transitionEnterTimeout={1300}
+          transitionLeaveTimeout={200}
+        >
           {imageExt.indexOf(fileExt) >= 0 ? image : videoExt.indexOf(fileExt) >= 0 ? video : iframe}
-        </div>
+        </CSSTransitionGroup>
       </div>
     )
   }
